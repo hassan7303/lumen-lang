8c6ee5673820e1cfc5604bb50b404892b2a229d7
hsnali <hassanali7303@gmail.com>
2025-02-04 14:40:41 +0330

    complite package lumen lang

---
+++ src/Providers/ValidationServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace LumenValidationFA;
+
+use Illuminate\Support\ServiceProvider;
+
+class ValidationServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $bootstrapPath = base_path('bootstrap/app.php');
+
+        $content = file_get_contents($bootstrapPath);
+        if (!str_contains($content, '$app->setLocale(')) {
+            $newContent = str_replace(
+                'return $app;',
+                "\$app->setLocale(env('APP_LOCALE', 'fa'));\n\nreturn \$app;",
+                $content
+            );
+
+            file_put_contents($bootstrapPath, $newContent);
+        }
+
+        $langPath = base_path('resources/lang/fa');
+        if (!is_dir($langPath)) {
+            mkdir($langPath, 0755, true);
+        }
+        
+        $packageLangFile = __DIR__ . '/translations/fa.php';
+
+        $destinationLangFile = $langPath . '/validation.php';
+
+        if (!file_exists($destinationLangFile)) {
+            copy($packageLangFile, $destinationLangFile);
+        }
+    }
+}
